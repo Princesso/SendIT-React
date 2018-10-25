abff8b1f9b372fe0e50f4159c9480902755d50fd
princesso <egbunaoluebube@gmail.com>
2018-10-25 11:20:22 +0100

    add app.js and index.js

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ msg: 'Hello Boo'});
+});
+
+
+app.listen(8080, () => console.log('app stareted on port 8080'));
