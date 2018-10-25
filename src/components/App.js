924300464d668f5b91ae303d4e69b19ab46bf3f3
princesso <egbunaoluebube@gmail.com>
2018-10-25 11:16:59 +0100

    write start-client, start-server, start-concurrently scripts, install babel/node, concurrently, babel-cli, body parser cors and express

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 class App extends Component{
+  componentDidMount() {
+    setTimeout(() => {
+      fetch('http://localhost:8080/', {
+        'Access-Control-Allow-Origin': '*'
+      }).then((res) => {
+        return res.json()
+      })
+      .then((res) => console.log({res}))
+    }, 5000)
+  }
   render(){
    return(
     <div>
