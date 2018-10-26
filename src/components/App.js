68e8048da8bfab48d0e72e5914d7105f268dcc28
princesso <egbunaoluebube@gmail.com>
2018-10-26 12:57:40 +0100

    update routes path, links and components in

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import HomePage from '../components/pages/homepage'
+import Login from '../components/pages/login'
+ 
 class App extends Component{
   componentDidMount() {
     setTimeout(() => {
@@ -11,11 +14,15 @@ class App extends Component{
       .then((res) => console.log({res}))
     }, 5000)
   }
+
   render(){
    return(
-    <div>
-    <h1>Yaay</h1>
-  </div>
+     <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={Login} />   
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+     </BrowserRouter>
    );
   }
 }
