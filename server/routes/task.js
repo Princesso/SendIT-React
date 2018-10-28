d19c63d3daf059668b9fddd11a6b4de75207cf62
princesso <egbunaoluebube@gmail.com>
2018-10-28 17:51:20 +0100

    create route for tasks

---
+++ server/routes/task.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var Joi = require('joi');
+var bodyParser = require('body-parser');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('task', { title: 'Tasks' });
+});
+
+module.exports = router;
