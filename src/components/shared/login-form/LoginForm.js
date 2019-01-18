493d7ba66d027da8cef6fccb217db2a920ce1194
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-18 19:27:06 +0100

    Implement login with redux

--- src/components/shared/login-form/LoginForm.js
+++ src/components/shared/login-form/LoginForm.js
@@ -1,20 +1,73 @@
 import React from 'react'
 import './login-form.css'
 import '../../../styles/main.css'
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {userLogin} from '../../../actions/authActions'
 
-const LoginForm = () => {
-  return(
-    <div className="form-holder">
-      <form className="form"> 
-        <h3>Login</h3>
-        <input type="email" placeholder="Email" required id="email"></input>
-        <input type="password" placeholder="Password" required  id="password"></input>
-        <button type="submit" id="submitBtn">Login</button>
-        <p id="message">Have no account? <Link to="/register" className="link"> Register</Link></p>
-      </form>
-    </div>
-  )
+class LoginForm extends React.Component {
+  state = {
+    email: '',
+    password:'',
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit= async(e) => {
+    e.preventDefault();
+    const loginData = this.state;
+    //write validation function
+    this.setState({
+      email: '',
+      password:'',
+    })
+    this.props.userLogin(loginData)
+  }
+
+  componentDidMount() {
+    if(this.props.auth.isAuthenticated === true)
+    this.props.history.push('/dashboard') 
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps)
+    if(nextProps.auth.isAuthenticated === true)
+    this.props.history.push('/dashboard') 
+  }
+  render() {
+    const {email, password} = this.state
+    return(
+      <div className="form-holder">
+        <form className="form" method="post" onSubmit={this.handleSubmit}> 
+          <h3>Login</h3>
+          <input 
+            type="email" 
+            placeholder="Email" 
+            required 
+            id="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            required  
+            id="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <button type="submit" id="submitBtn">Login</button>
+          <p id="message">Have no account? <Link to="/register" className="link"> Register</Link></p>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default LoginForm
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {userLogin}) (withRouter(LoginForm))
