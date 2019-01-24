0a0b92d39c9d08a30146145f1b64e5fbe0854857
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-01-24 14:45:42 +0100

    Update header links from a tag to react Link tag

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -2,16 +2,17 @@ import React from 'react';
 import styled from 'styled-components'
 import colors from '../../../styles/colors'
 import './header.css'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return(
     <div className="header">
-      <div > Send-It</div>
+      <div className="logo"> Send-It</div>
       <div>
         <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Login</li>
+        <li><Link to="/" className="link">HOME</Link></li>
+        <li><Link to="/" className="link">ABOUT</Link></li>
+        <li><Link to="/login" className="link">LOGIN</Link></li>
         </ul>
       </div>
     </div>
